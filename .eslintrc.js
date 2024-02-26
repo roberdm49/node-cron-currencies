83f9c9b74ab6b3769796a5908b60f63ad8e89194
Roberto Marcos <roberdm49@gmail.com>
2024-02-26 16:05:29 -0300

    script to check .env

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
   extends: ['./node_modules/ts-standard/eslintrc.json'],
   parserOptions: {
     project: './tsconfig.json'
+  },
+  rules: {
+    // to use "if (someString) | if (!someOtherString)"
+    '@typescript-eslint/strict-boolean-expressions': 'off'
   }
 }
