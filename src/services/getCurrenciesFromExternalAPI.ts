d0bd17cf0f019e23958b71d84e2a170c5112684d
Roberto Marcos <roberdm49@gmail.com>
2024-02-29 10:53:32 -0300

    mock data for service in dev mode

--- src/services/getCurrenciesFromExternalAPI.ts
+++ src/services/getCurrenciesFromExternalAPI.ts
@@ -1,8 +1,29 @@
+import fs from 'fs'
+import path from 'path'
 import axios, { AxiosResponse } from 'axios'
+import chalk from 'chalk'
 import { CurrencyData } from '@/types'
 import { CURRENCY_PATHS, GlobalEnv } from '@/utils/constants'
 
 export const getCurrenciesFromExternalAPI = async (): Promise<CurrencyData[]> => {
+  if (GlobalEnv.NODE_ENV === 'development') {
+    return await getDataFromMock()
+  }
+
+  return await getDataFromAPI()
+}
+
+const getDataFromMock = async (): Promise<CurrencyData[]> => {
+  console.log(chalk.yellowBright('Retrieving data from mock'))
+
+  const mockFilePath = path.join(__dirname, '../mock/currenciesFromExternalApi.json')
+  const mockData = fs.readFileSync(mockFilePath, 'utf8')
+  return JSON.parse(mockData)
+}
+
+const getDataFromAPI = async (): Promise<CurrencyData[]> => {
+  console.log(chalk.cyanBright('Retrieving data from external API'))
+
   const currencyPromises = []
 
   for (const currencyPath of Object.values(CURRENCY_PATHS)) {
